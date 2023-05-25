033ab55cabb8cb7db1029ed59dd53caa58a0d1a9
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-05-25 17:56:13 +0100

    Added tags to plants.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,15 @@
 
 puts "Destroying all gardens.."
 Garden.destroy_all
+Tag.destroy_all
 
 puts "Creating gardens.."
 garden1 = Garden.create!(name: "Tropicalia", banner_url: "https://www.thespruce.com/thmb/rv6B9HhTZD3SADvwVDtTiEb1pDs=/3600x0/filters:no_upscale():max_bytes(150000):strip_icc()/amaranthgarden-5839d38f5f9b58d5b14570f0.jpg")
 garden2 = Garden.create!(name: "Flowerina", banner_url: "https://img.freepik.com/premium-photo/multicolored-flower-bed-garden-3d-render-raster-illustration_607202-776.jpg?w=2000")
 
+puts "Creating tags..."
+Tag.create(name: "Toxic")
+Tag.create(name: "Smelly")
+Tag.create(name: "Bright")
 
 puts "Finished!"
